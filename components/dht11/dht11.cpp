4023be8367ff71da8ba562eff50240b89ade76b1
Ivan Pogiba <ipogiba@ozon.ru>
2024-11-10 00:39:05 +0300

    [PM] add dht-11 dir

---
+++ components/dht11/dht11.cpp
@@ -0,0 +1,7 @@
+//
+// Created by ipogiba on 10.11.2024.
+//
+
+#include "dht11.h"
+
+
---
+++ components/dht11/include/dht11.h
@@ -0,0 +1,15 @@
+//
+// Created by ipogiba on 10.11.2024.
+//
+
+#ifndef DHT11_H
+#define DHT11_H
+
+
+
+class dht11 {
+
+};
+
+
+#endif //DHT11_H
